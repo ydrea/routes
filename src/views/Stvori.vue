5f49320636491fe4a0eab8f7e33a90229520cb63
ydrea <55543450+ydrea@users.noreply.github.com>
2021-03-03 12:57:21 +0100

    first commit

---
+++ src/views/Stvori.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-for="stvor in stvorovi" :key="stvor.id"> 
+<h2> {{stvor.ko}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+data () {
+    return {
+        stvor: [
+            {ko: 'medo', di: 'usumu', id: 1},
+            {ko: 'zeko', di: 'uvrtu', id: 2},
+            {ko: 'jede', di: 'mukve', id: 3},
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
