ff3d574436c51a672668bb3d7a5a06bb3b7bac10
ydrea <55543450+ydrea@users.noreply.github.com>
2021-03-03 17:47:01 +0100

    a

---
+++ src/views/stvor/Stvorovi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+       Koji je ovo param? {{ id }} </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
